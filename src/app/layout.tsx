8454df8d6d7fd2b5462e7bceee77e2010a671e74
G.gorfor <rawiponponsarutwanit@gmail.com>
2025-11-27 00:54:19 +0700

    Feat : theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,33 @@ import "@/styles/globals.css";
 import { type Metadata } from "next";
 import { TRPCReactProvider } from "@/trpc/react";
 import { nunito, kodchasan } from "@/constants/fonts";
+import { ThemeProvider } from "@/components/theme-provider"; // Import ที่สร้างตะกี้
 
 export const metadata: Metadata = {
   title: "Rawipon | Portfolio",
   description: "Mobile & Web Developer Portfolio",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${nunito.variable} ${kodchasan.variable}`}>
-      <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+
+    <html
+      lang="en"
+      className={`${nunito.variable} ${kodchasan.variable}`}
+      suppressHydrationWarning
+    >
+      <body className="min-h-screen bg-white text-slate-900 transition-colors duration-300 dark:bg-slate-950 dark:text-slate-200">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
